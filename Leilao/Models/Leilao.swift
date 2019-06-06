1b9ebc80c687e3607b35621a113e36cd15781a2f
Lucas Góes Valle <lgoesvalle@gmail.com>
2019-06-05 22:22:29 -0300

    TDD and refactor

--- Leilao/Models/Leilao.swift
+++ Leilao/Models/Leilao.swift
@@ -24,8 +24,27 @@ class Leilao {
 
         guard let listaDeLances = lances else { return }
 
-        if listaDeLances.count == 0 || listaDeLances.last?.usuario != lance.usuario {
+        if listaDeLances.count == 0 || podeDarLance(lance.usuario, listaDeLances){
             lances?.append(lance)
         }
+    }
+
+    private func ultimoLance(_ lances: [Lance]) -> Lance? {
+        return lances.last
+    }
+
+    private func podeDarLance(_ usuario: Usuario, _ lances: [Lance]) -> Bool {
+        return ultimoLance(lances)?.usuario != usuario && quantidadeDeLancesDoUsuario(usuario) < 5
+    }
+
+    private func quantidadeDeLancesDoUsuario(_ usuario: Usuario) -> Int {
+        guard let listaDeLances = lances else { return 0 }
+        var total = 0
+        for lanceAtual in listaDeLances {
+            if lanceAtual.usuario == usuario {
+                total = total.advanced(by: 1)
+            }
+        }
+        return total
     }
 }
